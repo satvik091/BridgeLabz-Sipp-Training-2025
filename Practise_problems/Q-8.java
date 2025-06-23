2afc8f4d25fd794482ee9f18f7554ee7a11ac46f
Satvik Gupta <satvikg21111124@gmial.com>
2025-06-23 14:14:34 +0530

    Remove extra problems

--- Practise_problems/Q-8.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class Main{
-public static void main(String arg[]){
-Scanner sc=new Scanner(System.in);
-int Kilometers=sc.nextInt();
-double Miles = Kilometers * 0.621371;
-System.out.print(Miles);
-}
-}
